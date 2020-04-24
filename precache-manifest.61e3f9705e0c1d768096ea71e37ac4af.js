972b617546719dd7fcc83ac39be9642fafebf743
ddayan0 <denday198@gmail.com>
2020-04-23 21:47:06 -0400

    Updates

---
+++ precache-manifest.61e3f9705e0c1d768096ea71e37ac4af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a81a32928fc4be6905adecdf53554db",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "cddc5a4b7f3358783af9",
+    "url": "/personal-website/static/css/2.53431924.chunk.css"
+  },
+  {
+    "revision": "f1ee1632224272f8a72a",
+    "url": "/personal-website/static/css/main.8b5b7d0c.chunk.css"
+  },
+  {
+    "revision": "cddc5a4b7f3358783af9",
+    "url": "/personal-website/static/js/2.482522ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal-website/static/js/2.482522ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ee1632224272f8a72a",
+    "url": "/personal-website/static/js/main.b5e6353b.chunk.js"
+  },
+  {
+    "revision": "d63440558f5c67648dd5",
+    "url": "/personal-website/static/js/runtime-main.9fc4fff5.js"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/personal-website/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/personal-website/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/personal-website/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/personal-website/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
